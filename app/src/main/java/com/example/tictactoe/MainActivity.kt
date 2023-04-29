032297a07ce3c8ad1788c306aebb0882e4a9fa27
Vishwas <VishwasDubey150@gmail.com>
2023-04-29 16:12:59 +0530

    first commit

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+    }
+
+    fun play(view: View) {}
+}
